4ae52f9473cc83e36a116dd3172cc48eb4b7559a
hebele <seymabob133@gmail.com>
2026-08-15 02:31:25 +0300

    bugünün siftahı

--- ga/scripts/Map.gd
+++ ga/scripts/Map.gd
@@ -3,9 +3,12 @@ class_name Map extends RefCounted
 const Width := 256
 const Height := 256
 
+const YAYILIM_HIZI := 2
+
 
 
 var nem:       PackedFloat32Array
+var nem_update:PackedFloat32Array
 var gunes:     PackedFloat32Array
 var verim:     PackedFloat32Array
 
@@ -14,9 +17,23 @@ func _init() -> void:
 	nem.resize(n)
 	gunes.resize(n)
 	verim.resize(n)
+	nem_update.resize(n)
 
 	
 var img = Image.create(Width,Height,false,Image.FORMAT_RGB8)	
 
 func produce_ground():
 	pass
+	
+func yayilim_fonksiyonu(dt: float) -> void:
+	var water_out_coef = min(YAYILIM_HIZI*dt,0.2)
+	for y in Height:
+		for x in Width:
+			var i := y*Width+x
+			water_out = nem[i] *water_out_coef
+			
+				nem_update[i] -= water_out*4
+				nem_update[i-1] += water_out
+				nem_update[i+1] += water_out
+				nem_update[i - Width] += water_out
+				nem_update[i + Width] += water_out
--- ga/scripts/world.gd
+++ ga/scripts/world.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+const REFRESH_SPEED := 10.0 #increase to make game faster
+
+const STEP_SIZE := 1.0/REFRESH_SPEED
+var accumlation := 0.0
 var map : Map
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,8 +13,17 @@ func _ready() -> void:
 	var ground := $Ground
 	print(ground.tile_set)
 
-	for x in map.Width:
-		for y in map.Height:
+	for y in map.Height:
+		for x in map.Width:
 			ground.set_cell(Vector2i(x, y), 0, Vector2i(0, 0))
 	print(ground.get_used_cells().size())
-			
+	ground.set_cell(Vector2i(0, 0), 0, Vector2i(1, 0))
+	ground.set_cell(Vector2i(1, 0), 0, Vector2i(1, 0))
+	ground.set_cell(Vector2i(0, 1), 0, Vector2i(1, 0))
+	
+func _process(delta: float) -> void:
+	accumlation += delta
+	while accumlation > (STEP_SIZE):
+		accumlation -= (STEP_SIZE)
+		map.yayilim_fonksiyonu(STEP_SIZE)
+	
