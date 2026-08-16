f9667e17b7e763d2a93ede892e35acf1e5708204
hebele <seymabob133@gmail.com>
2026-08-16 21:36:12 +0300

    fonksiyon update
    verim ve yayılım fonkları eklendi

--- ga/scripts/Map.gd
+++ ga/scripts/Map.gd
@@ -6,11 +6,17 @@ const Height := 256
 const YAYILIM_HIZI := 2
 
 
+const VERIM_ESIK := 0.3
+const VERIM_ARTIS := 0.05
+const VERIM_AZALIS := 0.02
+
 
 var nem:       PackedFloat32Array
 var nem_update:PackedFloat32Array
 var gunes:     PackedFloat32Array
 var verim:     PackedFloat32Array
+
+
 
 func _init() -> void:
 	var n = Width * Height
@@ -22,18 +28,42 @@ func _init() -> void:
 	
 var img = Image.create(Width,Height,false,Image.FORMAT_RGB8)	
 
+
+
 func produce_ground():
 	pass
 	
+	
+func verim_guncelleme(dt: float) -> void:
+	for i in verim.size():
+		if nem[i] > VERIM_ESIK:
+			verim[i] += VERIM_ARTIS * (nem[i] - VERIM_ESIK) / (1.0 - VERIM_ESIK) * dt
+		elif nem[i] < VERIM_ESIK:
+			verim[i] -= VERIM_AZALIS * (VERIM_ESIK - nem[i]) / VERIM_ESIK * dt
+		verim[i] = clampf(verim[i], 0.0, 1.0)
+		
 func yayilim_fonksiyonu(dt: float) -> void:
+	nem_update.fill(0.0)
 	var water_out_coef = min(YAYILIM_HIZI*dt,0.2)
 	for y in Height:
 		for x in Width:
 			var i := y*Width+x
-			water_out = nem[i] *water_out_coef
+			var water_out = nem[i] *water_out_coef
+			var count := 0
 			
-				nem_update[i] -= water_out*4
-				nem_update[i-1] += water_out
-				nem_update[i+1] += water_out
+			if x > 0:				
+				nem_update[i - 1] += water_out
+				count +=1
+			if x < Width - 1:
+				nem_update[i + 1] += water_out
+				count +=1
+			if y > 0:
 				nem_update[i - Width] += water_out
+				count +=1
+			if y < Height - 1:
 				nem_update[i + Width] += water_out
+				count +=1
+			nem_update[i] -= count*water_out
+			
+	for j in nem.size():
+		nem[j] += nem_update[j]
